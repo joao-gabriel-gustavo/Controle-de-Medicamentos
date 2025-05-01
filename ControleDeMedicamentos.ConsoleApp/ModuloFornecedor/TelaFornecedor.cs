6e934e1f3bceaf8c660c19dfac2e40e95fa48b08
João <joaog.alves14@gmail.com>
2025-05-01 16:55:41 -0300

    Corrige formato do CNPJ exibido na tela do fornecedor

--- ControleDeMedicamentos.ConsoleApp/ModuloFornecedor/TelaFornecedor.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloFornecedor/TelaFornecedor.cs
@@ -18,7 +18,7 @@ public class TelaFornecedor : TelaBase<Fornecedor>, ITelaCrud
         ContextoDados contexto = new ContextoDados();
         RepositorioFornecedorEmArquivo repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
 
-        Console.Write("Digite o CNPJ no formato: XX.XXX.XXX/XXX-XX");
+        Console.Write("Digite o CNPJ no formato: XX.XXX.XXX/XXXX-XX");
         string cnpj = Console.ReadLine()!;
 
         bool cnpjExiste = false;
