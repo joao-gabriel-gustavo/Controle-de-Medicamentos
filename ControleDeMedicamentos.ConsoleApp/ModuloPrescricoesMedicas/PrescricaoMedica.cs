0408b18ee02ce27a3fd6210fb6a81428a7e0e292
Gustavo Duara <gugaduara@gmail.com>
2025-05-05 15:48:07 -0300

    criado modulo prescricao medica

--- ControleDeMedicamentos.ConsoleApp/ModuloPrescricoesMedicas/PrescricaoMedica.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPrescricoesMedicas/PrescricaoMedica.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloPaciente;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace ControleDeMedicamentos.ConsoleApp.ModuloPrescricoesMedicas
 {
-    class PrescricaoMedica
+    public class PrescricaoMedica : EntidadeBase<PrescricaoMedica>
     {
+        public string CRM { get; set; }
+        public DateTime DataPrescricao { get; set; }
+
+        public override void AtualizarRegistro(PrescricaoMedica registroEditado)
+        {
+            CRM = registroEditado.CRM;
+            DataPrescricao = registroEditado.DataPrescricao;
+        }
+
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
